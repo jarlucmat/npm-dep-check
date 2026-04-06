4a04a58ca38fbeb2dbdce72d86d28664f2c97866
jarlucmat <15339861+jarlucmat@users.noreply.github.com>
2026-04-06 14:04:13 +0200

    Escape user input

--- functions/__npm_dep_check_queryPackageName.fish
+++ functions/__npm_dep_check_queryPackageName.fish
@@ -1,5 +1,6 @@
 function __npm_dep_check_queryPackageName
-	set -f searchString "^$(string replace -a '*' '.*' -- $argv[1]):.*\$"
+	set -f escapedInput (string escape --style=regex -- $argv[1])
+	set -f searchString "^$(string replace -a '\*' '.*' -- $escapedInput):.*\$"
 	string match -r -- $searchString $__npmDepCheck_NPM_PACKAGE_LIST_CACHE |\
 		string split -f 1 --allow-empty ':' |\
 		sort -u
